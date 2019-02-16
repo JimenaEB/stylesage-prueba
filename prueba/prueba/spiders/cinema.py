196efeaab62649a6f28acfa6f28cd6a910be1acf
Jimena <jime.ebermudez@gmail.com>
2019-02-16 01:45:06 +0100

    Update docstrings

--- prueba/prueba/spiders/cinema.py
+++ prueba/prueba/spiders/cinema.py
@@ -5,7 +5,7 @@ class CinemaSpider(scrapy.Spider):
     name = "cinema"
 
     def start_requests(self):
-        """Envía la petición al endpoint que devuelve los datos"""
+        """Send the petition to the endpoint that returns the cinema data"""
         url = 'http://www.yelmocines.es/now-playing.aspx/GetNowPlaying'
         yield scrapy.Request(
             url=url, callback=self.parse, method='POST',
@@ -14,24 +14,24 @@ class CinemaSpider(scrapy.Spider):
         )
 
     def parse(self, response):
-        """Extrae la información de la respuesta"""
+        """Extract the data from response"""
         info = json.loads(response.text)
         cinemas = info.get('d').get('Cinemas')
-        
+
         for cinema in cinemas:
             yield self.parse_cinemas(cinema)
 
     def parse_cinemas(self, cinema):
-        """Extrae la información de un cine"""
+        """Extract the data from a single cinema"""
         cine = {
-            'cinema_name' : cinema.get('Name'),
+            'cinema' : cinema.get('Name'),
             'movies' : self.get_movies(cinema.get('Dates'))
         }
         print(json.dumps(cine))
         return cine
 
     def get_movies(self, dates):
-        """Extrae la información de todas las películas de un cine"""
+        """Extract all the data from the movies at a single cinema"""
         parsed_movies = {}
 
         for date in dates:
@@ -42,9 +42,9 @@ class CinemaSpider(scrapy.Spider):
                     parsed_movies[movie.get('Title')]['show_date'].append(show_date)
                 else:
                     parsed_movies[movie.get('Title')] = {
-                        'rating': movie.get('RatingDescription'),
-                        'gender': movie.get('Gender'),
-                        'sypnosis': movie.get('Synopsis'),
+                        'rating': movie.get('RatingDescription') or None,
+                        'gender': movie.get('Gender') or None,
+                        'sypnosis': movie.get('Synopsis') or None,
                         'show_date': [show_date]
                     }
 
