20eb21ea31d1341b8eb0464bdb1f3881fc299333
Jimena <jime.ebermudez@gmail.com>
2019-02-17 19:40:27 +0100

    Change dynamo sttings and update database for theatre spider

--- prueba/prueba/create_database.py
+++ prueba/prueba/create_database.py
@@ -28,6 +28,44 @@ def create_cinema_table(dynamodb):
     if table.item_count == 0:
         print("Tabla cinemas creada con éxito")
 
+def create_theatre_table(dynamodb):
+    """Create theatre table in dynamodb"""
+    table = dynamodb.create_table(
+        TableName='theatres',
+        KeySchema=[
+            {
+                'AttributeName': 'play_name',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'theatre',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'play_name',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'theatre',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 3
+        }
+    )
+
 if __name__ == "__main__":
     dynamodb = boto3.resource('dynamodb')
-    create_cinema_table(dynamodb)
+    try:
+        create_cinema_table(dynamodb)
+    except:
+        print("Cinema table already exists")
+
+    try:
+        create_theatre_table(dynamodb)
+    except:
+        print("Theatre table already exists")
